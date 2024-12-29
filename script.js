9478f85c5c8578f5e071747394013566cf950624
faizsheikh1608 <164844564+faizsheikh1608@users.noreply.github.com>
2024-12-29 10:34:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+// script.js
+const inputs = document.querySelectorAll(".code");
+
+inputs.forEach((input, index) => {
+  input.addEventListener("input", (e) => {
+    const currentInput = input;
+    const nextInput = inputs[index + 1];
+
+    // Move to next input if value is entered
+    if (currentInput.value && nextInput) {
+      nextInput.focus();
+    }
+  });
+
+  input.addEventListener("keydown", (e) => {
+    const currentInput = input;
+    const previousInput = inputs[index - 1];
+
+    if (e.key === "Backspace") {
+      if (currentInput.value === "" && previousInput) {
+        previousInput.focus();
+      }
+    }
+  });
+});
