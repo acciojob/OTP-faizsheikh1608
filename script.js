4034df6c6dcad8fb0a5e96586fb37b0e3764bcc6
faizsheikh1608 <164844564+faizsheikh1608@users.noreply.github.com>
2024-12-29 10:42:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-//your JS code here. If required.
-// script.js
-const inputs = document.querySelectorAll(".code");
+document.addEventListener("DOMContentLoaded", () => {
+  const inputs = document.querySelectorAll(".code");
 
-inputs.forEach((input, index) => {
-  input.addEventListener("input", (e) => {
-    const currentInput = input;
-    const nextInput = inputs[index + 1];
+  inputs.forEach((input, index) => {
+    input.addEventListener("input", (e) => {
+      const value = e.target.value;
 
-    // Move to next input if value is entered
-    if (currentInput.value && nextInput) {
-      nextInput.focus();
-    }
-  });
-
-  input.addEventListener("keydown", (e) => {
-    const currentInput = input;
-    const previousInput = inputs[index - 1];
-
-    if (e.key === "Backspace") {
-      if (currentInput.value === "" && previousInput) {
-        previousInput.focus();
+      if (value.length > 1) {
+        // If user pastes more than one character, take only the first character
+        e.target.value = value.charAt(0);
       }
-    }
+
+      if (value && index < inputs.length - 1) {
+        // Move to the next input if available
+        inputs[index + 1].focus();
+      }
+    });
+
+    input.addEventListener("keydown", (e) => {
+      if (e.key === "Backspace" && !e.target.value && index > 0) {
+        // Move to the previous input if it's empty
+        inputs[index - 1].focus();
+      }
+    });
   });
 });
